c29e238e10a4c4ae41567be1e08a6c692a85d3b3
yorik100 <yorik100@hotmail.com>
2023-03-19 16:34:18 +0100

    Rafael

--- OpenUtilities/utilities.cpp
+++ OpenUtilities/utilities.cpp
@@ -873,6 +873,9 @@ namespace utilities {
 			}
 		}
 
+		if (settings::fow::updatePos->get_bool() && obj->get_particle_attachment_object() && !obj->get_particle_attachment_object()->is_visible() && !obj->get_particle_attachment_object()->is_dead() && obj->get_particle_attachment_object()->get_position().is_valid() && obj->get_position().is_valid())
+			obj->get_particle_attachment_object()->set_position(obj->get_position());
+
 		// Get possible valid particles
 		if (!obj->get_emitter() || !obj->get_emitter()->is_enemy() || !obj->get_emitter()->is_ai_hero()) return;
 
